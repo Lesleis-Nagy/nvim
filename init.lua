87bb15b44358b75cc937a58a8a80ecb00788631c
Lesleis Nagy <lesleisnagy@gmail.com>
2025-09-18 12:23:43 +0100

    Changed debugging shortcuts

--- init.lua
+++ init.lua
@@ -450,11 +450,11 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 
 -- Key mappings for debugging
-vim.api.nvim_set_keymap('n', '<F5>', '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F10>', '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F11>', '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F12>', '<cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-c>', '<cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-n>', '<cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-i>', '<cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-o>', '<cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<M-b>', '<cmd>lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = true })
 
 --- --------------------------------------------------------------------- ---
 --- Useful things                                                         ---
